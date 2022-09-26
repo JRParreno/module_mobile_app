7f7282effe39b0cbf6d1fe490316891ba1d31f81
JRParreno <jhonrhayparreno22@gmail.com>
2022-09-26 12:51:16 +0800

    update data

---
+++ data/ENUMERATION.ts
@@ -0,0 +1,35 @@
+export default function ENUMERATION() {
+    // kindly match the quarter_pk in QUARTER.ts file
+    return [
+        {
+            "pk": "993221332",
+            "quarter_pk": "7113874452",
+            "image": require('../assets/lottie/amun_selection/102960-math-concept-6.json'),
+            "question": "What is the tagalog of Dog?",
+            "answer": "aso"
+        },
+        {
+            "pk": "9932213442",
+            "quarter_pk": "7113874452",
+            "image": require('../assets/lottie/amun_selection/102960-math-concept-6.json'),
+            "question": "What is the tagalog of Cat?",
+            "answer": "pusa"
+        },
+        {
+            "pk": "993221332",
+            "quarter_pk": "0214811335",
+            "direction": "Direction",
+            "image": require('../assets/lottie/amun_selection/102960-math-concept-6.json'),
+            "question": "What is the tagalog of Dog?",
+            "answer": "aso"
+        },
+        {
+            "pk": "993221332",
+            "quarter_pk": "0214811335",
+            "direction": "Direction",
+            "image": require('../assets/lottie/amun_selection/102960-math-concept-6.json'),
+            "question": "What is the tagalog of Dog?",
+            "answer": "aso"
+        },
+    ];
+}
--- data/QUARTER.ts
+++ data/QUARTER.ts
@@ -2,13 +2,13 @@ export default function QUARTER() {
     return [
         {
             "pk": "7113874452",
-            "category_pk": "6810921321",
             "title": "Quarter 1",
+            "direction": "Direction"
         },
         {
             "pk": "0214811335",
-            "category_pk": "6810921321",
             "title": "Quarter 2",
+            "direction": "Direction"
         },
     ]
 }
